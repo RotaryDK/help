0fcfb0a77c1846b9928937f7207f04c36a4b6367
Christian Pedersen <christian.pedersen@zentura.dk>
2023-03-27 16:07:55 +0200

    Added auto open browser

--- StartServerPython.ps1
+++ StartServerPython.ps1
@@ -5,5 +5,6 @@ if (Test-Path(".\env")) {
     . ".\env\Scripts\Activate.ps1"
     pip.exe install -r requirements.txt
 }
+Start-Process "http://127.0.0.1:8000/"
 Set-Location ".\src"
 mkdocs.exe serve
